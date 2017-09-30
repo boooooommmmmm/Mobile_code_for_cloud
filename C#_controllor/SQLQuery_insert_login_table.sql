b8c55a355665c0b97478163b23beb140f52250b7
boooooommmmmm <hushuyan88@gmail.com>
2017-09-30 15:04:44 +1000

    1
    1

---
+++ C#_controllor/SQLQuery_insert_login_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO LoginTable values(1, '123@gmail.com', '123'),(2, 'testAdminUser@gmail.com', 'AdminPassword') 
+
+
+SELECT * 
+FROM LoginTable
---
+++ C#_controllor/SQLQuery_login_table.sql
@@ -0,0 +1,6 @@
+ CREATE TABLE LoginTable(
+   ID   INT								,
+   Emial VARCHAR (50)				NOT NULL,
+   Password  VARCHAR (50)				NOT NULL,     
+   PRIMARY KEY (ID)
+);
