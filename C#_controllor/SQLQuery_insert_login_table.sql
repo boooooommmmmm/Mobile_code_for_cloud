2ab882fcc807f4ad90f9986df10fb5b0cbda88e7
boooooommmmmm <hushuyan88@gmail.com>
2017-09-30 18:36:51 +1000

    1
    1

--- C#_controllor/SQLQuery_insert_login_table.sql
+++ C#_controllor/SQLQuery_insert_login_table.sql
@@ -1,5 +1,1 @@
-INSERT INTO LoginTable values(1, '123@gmail.com', '123'),(2, 'testAdminUser@gmail.com', 'AdminPassword') 
-
-
-SELECT * 
-FROM LoginTable
+select * from TodoItems
---
+++ C#_controllor/TeamFantasyMobileAppService/Controllers/AcceleratorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+using Newtonsoft.Json.Linq;
+
+namespace TeamFantasyMobileAppService.Controllers
+{
+    [MobileAppController]
+    public class AcceleratorController : ApiController
+    {
+        string drivingState = "dangerous";
+        // GET api/Accelerator
+        public string Get(string json)
+        {
+            JObject JObjectJson = JObject.Parse(json);
+            int index = (int)(JObjectJson.GetValue("index"));
+            string name = (string)JObjectJson.GetValue("name");
+
+
+            return drivingState;
+        }
+    }
+}
