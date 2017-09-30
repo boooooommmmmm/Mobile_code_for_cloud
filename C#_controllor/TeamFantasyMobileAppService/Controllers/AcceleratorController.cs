695289cd0cb0ef3576767430dbf9f3ac13780c6a
boooooommmmmm <hushuyan88@gmail.com>
2017-09-30 23:42:57 +1000

    1
    1

--- C#_controllor/TeamFantasyMobileAppService/Controllers/AcceleratorController.cs
+++ C#_controllor/TeamFantasyMobileAppService/Controllers/AcceleratorController.cs
@@ -1,6 +1,11 @@
 ﻿using System.Web.Http;
 using Microsoft.Azure.Mobile.Server.Config;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace TeamFantasyMobileAppService.Controllers
 {
@@ -8,15 +13,38 @@ namespace TeamFantasyMobileAppService.Controllers
     public class AcceleratorController : ApiController
     {
         string drivingState = "dangerous";
+        int a = 0;
         // GET api/Accelerator
         public string Get(string json)
         {
-            JObject JObjectJson = JObject.Parse(json);
-            int index = (int)(JObjectJson.GetValue("index"));
-            string name = (string)JObjectJson.GetValue("name");
+
+            try
+            {
+                JObject jsonO = JObject.Parse(json);
+                //JObject jsonResultO = (JObject)jsonO["result"];
+                string a = jsonO.ToString();
+
+                return a;
+            }
+            catch (ParseException pe)
+            {
+                return "There is an error!";
+            }
+        }
+
+        //public Dictionary<string, int> AcceleratorRecord { get; set; }
+
+        public class AcceleratorRecord
+        {
+            public string x { get; set; }
+            public string y { get; set; }
+            public string z { get; set; }
+        }
 
 
-            return drivingState;
+        public class Record
+        {
+            public AcceleratorRecord record;
         }
     }
 }
