468c990c91b6f3710042d6eccd96ac2cfe9cb5a4
boooooommmmmm <hushuyan88@gmail.com>
2017-10-05 15:01:29 +1100

    EyeTrackingRecordController
    EyeTrackingRecordController

---
+++ C#_controllor/TeamFantasyMobileAppService/Controllers/EyeTrackingRecordController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace TeamFantasyMobileAppService.Controllers
+{
+    [MobileAppController]
+    public class EyeTrackingRecordController : ApiController
+    {
+        // GET api/EyeTrackingRecord
+        public string Get(string send)
+        {
+            return "Hello from custom controller!";
+        }
+    }
+}
